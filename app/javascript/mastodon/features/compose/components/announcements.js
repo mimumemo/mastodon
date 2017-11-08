0e2174d3cb64460913bb9095d7a03cd5c38bf7a6
mimumemo <guran3mimumemo@gmail.com>
2017-11-08 17:49:02 +0900

    Modify announcements data

--- app/javascript/mastodon/features/compose/components/announcements.js
+++ app/javascript/mastodon/features/compose/components/announcements.js
@@ -30,13 +30,13 @@ class Announcements extends React.PureComponent {
     const announcements = [];
     announcements.push(
       {
-        id: 16,
-        icon: '/announcements/info_05.png',
-        body: '祝ハロウィンミムメモ登場！',
+        id: 17,
+        icon: '/announcements/info_01.png',
+        body: '水古戦場お役立ち情報まとめ',
         link: [
           {
             inline: false,
-            href: 'https://gran-matome.com/archives/32897',
+            href: 'https://gran-matome.com/kosenjo-water',
             body: '記事を読む',
           },
         ],
@@ -53,7 +53,7 @@ class Announcements extends React.PureComponent {
           },
         ],
       }
-      // NOTE: id: 16 まで使用した
+      // NOTE: id: 17 まで使用した
     );
 
     this.announcements = Immutable.fromJS(announcements);
