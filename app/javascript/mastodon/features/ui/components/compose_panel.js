8d34e6a04f780806703887f59b713d8de65f9f1b
Masaru Ezaki <ezaki3@gmail.com>
2019-06-18 14:22:04 +0900

    Add Announcements and TrendTags in single-column mode

--- app/javascript/mastodon/features/ui/components/compose_panel.js
+++ app/javascript/mastodon/features/ui/components/compose_panel.js
@@ -3,13 +3,21 @@ import SearchContainer from 'mastodon/features/compose/containers/search_contain
 import ComposeFormContainer from 'mastodon/features/compose/containers/compose_form_container';
 import NavigationContainer from 'mastodon/features/compose/containers/navigation_container';
 import LinkFooter from './link_footer';
+import TrendTagsContainer from '../../compose/containers/trend_tags_container';
+import AnnouncementsContainer from '../../compose/containers/announcements_container';
 
 const ComposePanel = () => (
   <div className='compose-panel'>
-    <SearchContainer openInRoute />
-    <NavigationContainer />
-    <ComposeFormContainer />
-    <LinkFooter withHotkeys />
+    <div className='drawer__pager'>
+      <div className='drawer__inner'>
+        <SearchContainer openInRoute />
+        <NavigationContainer />
+        <ComposeFormContainer />
+        <AnnouncementsContainer />
+        <TrendTagsContainer />
+        <LinkFooter withHotkeys />
+      </div>
+    </div>
   </div>
 );
 
